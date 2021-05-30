fa2614e42bb155739666bda1c32aa035a7bf20a4
Corrine Tenglun Tan <tenglunt@gmail.com>
2021-05-30 21:37:52 +1000

    Update q1.sql

--- q1.sql
+++ q1.sql
@@ -1,8 +1,8 @@
-/* Question1: 
+/* Question 1: 
 Query information and course scores of students 
 with higher grades in the "01" course than in the "02" course*/
 
-/* SC table do a self-join(table a and b) first on sid to 
+/* Solution: SC table do a self-join(table a and b) first on sid to 
 get each student's cid in a same line, then filter table a(left side) 
 and table b(right side). Last do a outer 'in' query to get student info. */
 
