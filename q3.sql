30b490b51d15f07fbf258e14801a22be87a9c6f2
Corrine Tenglun Tan <tenglunt@gmail.com>
2021-05-30 23:17:44 +1000

    Update q3.sql

--- q3.sql
+++ q3.sql
@@ -1,8 +1,9 @@
 /* Question 3: 
 Query student information with grades in the SC table. */
 
-/* Solution: table.* will select every columns in table */
+/* Solution: table.* will select every columns in table. 
+Put 'distinct' to limit to 1 row per student information. */
 
-select student.* from student
+select distinct student.* from student
 inner join sc
 on sc.sid=student.sid
