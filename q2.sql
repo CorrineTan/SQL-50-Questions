f4c800742a5b0d47e5b7c20137cd88984d58d492
Corrine Tenglun Tan <tenglunt@gmail.com>
2021-05-30 23:01:50 +1000

    Update q2.sql

--- q2.sql
+++ q2.sql
@@ -2,9 +2,7 @@
 Query the student number and student name and average grade of students 
 with an average score greater than or equal to 60 points*/
 
-/* Solution: SC table do a self-join(table a and b) first on sid to 
-get each student's cid in a same line, then filter table a(left side) 
-and table b(right side). Last do a outer 'in' query to get student info. */
+/* Solution: use 'having' with the condition, not 'where', when there is a groupby clause */
 
 select student.sid, student.sname, avg(sc.score) as avg_score from student 
 inner join sc
