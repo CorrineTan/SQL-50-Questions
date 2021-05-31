6e016970922ab256531c13f32ae822a9a432fde6
Corrine Tenglun Tan <tenglunt@gmail.com>
2021-05-31 10:38:47 +1000

    Create q8.sql

---
+++ q8.sql
@@ -0,0 +1,15 @@
+/* Question 8: 
+Query the information of at least one class 
+that is the same as the student whose student ID is "01" */
+
+/* Solution: nothing specific. */
+
+select distinct student.*
+from sc 
+inner join student
+on sc.sid = student.sid
+where cid in (
+select cid
+from sc
+where sid = 01
+)
